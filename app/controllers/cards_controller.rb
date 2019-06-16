d198f098f8fd50ae2b9a0d09daf639c27eb289b1
yamamoto1108 <kjrps0826@gmail.com>
2019-06-16 15:40:04 +0900

    set_cards_create

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,13 +1,16 @@
 class CardsController < ApplicationController
   before_action :set_board
-  
+
   def show
   end
 
   def new
+    @card = Card.new
   end
 
   def create
+    @card = Card.create(card_params)
+    redirect_to board_path(@board)
   end
 
   def edit
@@ -23,5 +26,9 @@ class CardsController < ApplicationController
 
   def set_board
     @board = Board.find(params[:board_id])
+  end
+
+  def card_params
+    params.require(:card).permit(:name, :about, :deadline, :finish, :list_id)
   end
 end
