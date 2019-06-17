1c2bb17be9fa1ddef2496af93b7267fdc0484037
yamamoto1108 <kjrps0826@gmail.com>
2019-06-17 19:24:10 +0900

    set association

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,5 +2,6 @@ class Board < ApplicationRecord
   belongs_to :background
   has_many :lists, dependent: :destroy
   has_many :cards, through: :lists
+  belongs_to :user
   validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :boards
+
+  validates :name, presence: true
 end
