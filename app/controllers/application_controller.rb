246806ed0dd98927b17d9bd7a30df6ee5d1d78e5
yamamoto1108 <kjrps0826@gmail.com>
2019-06-18 21:18:46 +0900

    set devise_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :about])
+  end
+
   def after_sign_out_path_for(resource)
-    '/home/index'
+    root_path
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(@user)
   end
 end
