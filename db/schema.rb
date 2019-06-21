125f5db1396794e681708b9f3daddcd4f01f826a
yamamoto1108 <kjrps0826@gmail.com>
2019-06-21 19:36:10 +0900

    set team model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_21_092318) do
+ActiveRecord::Schema.define(version: 2019_06_21_103355) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -78,6 +78,22 @@ ActiveRecord::Schema.define(version: 2019_06_21_092318) do
     t.index ["board_id"], name: "index_lists_on_board_id"
   end
 
+  create_table "team_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "team_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_team_users_on_team_id"
+    t.index ["user_id"], name: "index_team_users_on_user_id"
+  end
+
+  create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "about"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "about"
@@ -97,4 +113,6 @@ ActiveRecord::Schema.define(version: 2019_06_21_092318) do
   add_foreign_key "card_users", "users"
   add_foreign_key "cards", "lists"
   add_foreign_key "lists", "boards"
+  add_foreign_key "team_users", "teams"
+  add_foreign_key "team_users", "users"
 end
