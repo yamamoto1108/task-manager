9dc859fa47e3d6c017e10a24b19b93e2b84f0ba5
yamamoto1108 <kjrps0826@gmail.com>
2019-06-21 18:19:11 +0900

    add user_id to boards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_085710) do
+ActiveRecord::Schema.define(version: 2019_06_21_091711) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2019_06_20_085710) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "background_id"
+    t.integer "user_id"
   end
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
