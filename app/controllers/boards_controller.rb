6beb3cc60aebf2c0282119c7558037a6743a7819
yamamoto1108 <kjrps0826@gmail.com>
2019-07-02 15:29:51 +0900

    show user.boards on boards/index

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,7 @@ class BoardsController < ApplicationController
   before_action :set_board, only: [:show, :edit, :update, :destroy]
 
   def index
-    @boards = Board.all
+    @pboards = Board.where(user_id: current_user.id, team_id: nil)
   end
 
   def show
