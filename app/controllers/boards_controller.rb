cc515a585e125007c4749e6a59e760cf77d47908
yamamoto1108 <kjrps0826@gmail.com>
2019-07-02 14:19:23 +0900

    add team_select on board-form

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -45,7 +45,7 @@ class BoardsController < ApplicationController
   private
 
   def board_params
-    params.require(:board).permit(:name, :about, :deadline, :finish, :background_id).merge(user_id: current_user.id, team_id: params[:team_id])
+    params.require(:board).permit(:name, :about, :deadline, :finish, :background_id, :team_id).merge(user_id: current_user.id)
   end
 
   def set_board
