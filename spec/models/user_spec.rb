50cb747ecfacb51df2e8b3d9efb6e07f091be1f9
yamamoto1108 <kjrps0826@gmail.com>
2019-08-08 17:58:56 +0900

    users.rb test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,46 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
+    it "is valid with a name, email, password, password_confirmation" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+
     it "is invalid without a name" do
       user = build(:user, name: "")
       user.valid?
       expect(user.errors[:name]).to include("を入力してください")
+    end
+
+    it "is invalid without an email" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
+
+    it "is invalid without a password" do
+      user = build(:user, password: "")
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
+
+    it "is invalid without a password_confirmation" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とPasswordの入力が一致しません")
+    end
+
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
+    end
+
+    it "is invalid with a password less than 5 characters" do
+      user = build(:user, password: "aaaaa")
+      user.valid?
+      expect(user.errors[:password][0]).to include("は6文字以上で入力してください")
     end
   end
 end
