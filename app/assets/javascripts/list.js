66c807aa5229c076bdac0bd2fd352a52c8a74327
yamamoto1108 <kjrps0826@gmail.com>
2019-08-11 13:08:41 +0900

    fix list ajax
    ボード最初のリストが非同期で反映しない現象を修正。
    非同期で作成したリストにアイコン追加

--- app/assets/javascripts/list.js
+++ app/assets/javascripts/list.js
@@ -1,34 +1,40 @@
-$(function() {
-  $(document).on('turbolinks:load', function() {
-    function buildHtml(list) {
-      var html = `<div class="list">
-                    <div class="list__name">
-                      ${list.name}
-                    </div>
-                  </div>`
-      return html;
-    }
-    $('#new_list').on('submit', function(e) {
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action')
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(data) {
-        var html = buildHtml(data);
-        $('.list__items').append(html);
-        $('.list__form').val('');
-      })
-      .fail(function() {
-        alert('error');
-      })
-      return false;
-    });
+$(document).on('turbolinks:load', function() {
+  function buildHtml(list) {
+    var html = `<div class="list">
+                  <div class="list__name">
+                    ${list.name}
+                  </div>
+                  <div class="list__icon">
+                    <a data-remote="true" href="/boards/${list.board_id}/lists/${list.id}/edit">
+                      <i class="fas fa-edit icon"></i>
+                    </a>
+                    <a data-dismiss="modal" data-confirm="リストを削除しますか？" data-commit="DELETE" data-cancel="Cancel" title="DELETE List" rel="nofollow" data-method="delete" href="/boards/${list.board_id}/lists/${list.id}">
+                      <i class="fas fa-trash-alt icon"></i>
+                    </a>
+                  </div>
+                </div>`
+    return html;
+  }
+  $('#new_list').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildHtml(data);
+      $('.list__items').append(html);
+      $('.list__form').val('');
+    })
+    .fail(function() {
+      alert('error');
+    })
+    return false;
   });
 });
--- app/views/lists/create.json.jbuilder
+++ app/views/lists/create.json.jbuilder
@@ -1,1 +1,3 @@
+json.id @list.id
 json.name @list.name
+json.board_id @list.board.id
