1376066f283067fcd84f8977f31bc61719b948b6
yamamoto1108 <kjrps0826@gmail.com>
2019-08-12 20:40:46 +0900

    add image validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,17 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :name, presence: true
+  validate :image_presence
 
   has_one_attached :image
+
+  def image_presence
+    if image.attached?
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      errors.add(:image, 'ファイルを添付してください')
+    end
+  end
 end
