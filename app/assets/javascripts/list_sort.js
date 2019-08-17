385e0ca0455c89e14657c7f816689b6bf25367c8
yamamoto1108 <kjrps0826@gmail.com>
2019-08-17 11:58:50 +0900

    list-sortable

---
+++ app/assets/javascripts/list_sort.js
@@ -0,0 +1,16 @@
+$(document).on('turbolinks:load', function() {
+  $('.list__items').sortable({
+    update: function(e, ui){
+      let item = ui.item;
+      let item_data = item.data();
+      let params = {_method: 'put'};
+      params[item_data.modelName] = { row_order_position: item.index() }
+      $.ajax({
+        type: 'POST',
+        url: item_data.updateUrl,
+        dataType: 'json',
+        data: params
+      });
+    }
+  });
+});
