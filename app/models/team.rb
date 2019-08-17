bc8616a4192e86d082d91aa45d61669237d7f258
yamamoto1108 <kjrps0826@gmail.com>
2019-08-17 17:10:24 +0900

    delete image validation

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,15 +5,5 @@ class Team < ApplicationRecord
 
   has_one_attached :image
   validates :name, presence: true, length: { maximum: 15 }
-  #validate :image_presence
 
-  # def image_presence
-  #   if image.attached?
-  #     if !image.content_type.in?(%('image/jpeg image/png'))
-  #       errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
-  #     end
-  #   else
-  #     errors.add(:image, 'ファイルを添付してください')
-  #   end
-  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,17 +13,7 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :name, presence: true, length: { maximum: 10 }
-  validate :image_presence
 
   has_one_attached :image
 
-  def image_presence
-    if image.attached?
-      if !image.content_type.in?(%('image/jpeg image/png'))
-        errors.add(:image, 'にはjpegまたはpngファイルを添付してください')
-      end
-    else
-      errors.add(:image, 'ファイルを添付してください')
-    end
-  end
 end
