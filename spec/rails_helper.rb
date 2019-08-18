9068c79f0bbad91d78a233cf6a34da3dddefc46d
yamamoto1108 <kjrps0826@gmail.com>
2019-08-18 20:31:03 +0900

    post board test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rspec'
 
 require 'devise'
 require File.expand_path("spec/support/controller_macros.rb")
